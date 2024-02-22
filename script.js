de6056ed14c9be9fc51994cb0fb3370062482a8a
mdsiaofficial <mdsiaofficial@gmail.com>
2024-02-22 11:16:29 +0600

    making a weather app

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+
+const apiKey = "1dc02c2cda3d32eda98eede7405d0e42";
+const cityInput = document.getElementById("cityName").value;
+const cityInfo =`&q=${cityInput}&appid=${apiKey}`;
+// const city = `&q=dhaka&appid=${apiKey}`;
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+
+const searchBox = document.querySelector(".searchArea input");
+const searchBtn = document.querySelector(".searchArea button");
+
+console.log(city);
+
+async function checkWeather(city) {
+    const response = await fetch(apiURL + city);
+    var data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temperature").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+};
+searchBtn.addEventListener("click", function(){
+
+    checkWeather(searchBox.value);
+});
+
